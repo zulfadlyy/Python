c338ee4f6d310b3d9afb8968a80bd4daecec74f5
zoofatlee <zulfadlyyusoff@gmail.com>
2024-08-30 09:51:10 +0800

    Lessons on Strings

--- 01 Print.py
+++ 01 Print.py
@@ -1,4 +1,4 @@
-# << the # key omits that line from code execution
+# The '#' key omits that line from code execution
 #print("don't print this")
 print("I love pizza")
 print("It's really good!")
--- 04 Strings.py
+++ 04 Strings.py
@@ -1,7 +1,9 @@
 #These are some of the things you can do with Strings
 #1 Len short for length.
 
-name = "Bro Code"
+name = "bro Code"
+number = "123"
+mixed = "abc123"
 #This will print 8 as spaces are also counted.
 print(len(name))
 
@@ -9,6 +11,31 @@ print(len(name))
 
 #This find the letter "o" and returns 2.
 print(name.find("o"))
-#what if i add something
 
-te
+#Capitalize 1st Letter
+print(name.capitalize())
+
+#Uppercase the whole word
+print(name.upper())
+
+#Lowercase the whole word
+print(name.lower())
+
+#Check if STR is a number
+print(name.isdigit())
+print(number.isdigit())
+
+#Combination will result in FALSE
+print(mixed.isdigit())
+
+#Check if STR is an Alphabet
+print(name.isalpha())
+
+#Count (arguement)
+print(name.count("o"))
+
+#Find and Replace (from , to)
+print(name.replace("o","a"))
+
+#MultiplePrint
+print(name*3)
