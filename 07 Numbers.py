49d3efa9e18ee51894e5d6fb4e99aaa93a6268ec
zoofatlee <zulfadlyyusoff@gmail.com>
2024-08-30 10:22:55 +0800

    Numbers

---
+++ 07 Numbers.py
@@ -0,0 +1,17 @@
+#Import a module
+import math
+
+pi = 3.14
+ip = -3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))#Round to the nearest int
+print(math.ceil(pi)) #Round up
+print(math.floor(pi))#Round down
+print(abs(ip)) #absolute value is how far a number is away from 0
+print(pow(pi,2)) #To the Power of
+print(math.sqrt(pi)) #Square Root
+print(max(x,y,z)) #Returns the largest value
+print(min(x,y,z)) #Returns the smallest value
