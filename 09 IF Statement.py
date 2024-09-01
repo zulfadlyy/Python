6e2f39e2d97ee35dacacaffcd34b2143eb3f34e1
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-01 12:25:29 +0800

    IF Statements

---
+++ 09 IF Statement.py
@@ -0,0 +1,13 @@
+# IF statement = block of code that will execute if it's condition is TRUE.
+# elif statement = means else if. similar to if statement, but used after if statement.
+# else statement = used after if and elif.
+age = int(input("How old are you?: "))
+
+if age == 100:
+    print("You are a century old!")
+elif age >= 18:
+    print("You are an adult!")
+elif age <= 0:
+    print("You haven't been born yet!")
+else:
+    print(("You are a child!"))
