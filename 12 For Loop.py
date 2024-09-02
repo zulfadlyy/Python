5f6ed5230951f8dec5a079b5d818c26cc11a02e7
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-02 20:14:29 +0800

    For Loop

---
+++ 12 For Loop.py
@@ -0,0 +1,25 @@
+import time
+#For Loop = a statement that will execute it's block of code
+#           a limited amount of times
+#
+#           while loop = unlimited
+#           for loop = limited
+
+#This basically count 0 to 9 but i added +1 after i so it's now 1 to 10
+# for i in range(10):
+#     print(i+1)
+
+#Counts 50 to 100, but only print every 2 numbers
+# for i in range(50,100+1,2):
+#     print(i)
+
+#Prints each letter as an individual line from the string.
+# for i in "Bro Code":
+#     print(i)
+
+
+#Countdown 10 to 0, -1 is decending, time.sleep acts like a delay. Prints Happy New Year when count is 0.
+# for seconds in range(10,0,-1):
+#     print(seconds)
+#     time.sleep(1)
+# print("Happy New Year!")
