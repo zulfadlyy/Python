19284b7f34f7e6051e2d3b6a21f2926ac650e3d6
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-02 20:29:53 +0800

    Nested Loop

---
+++ 13 Nested Loop.py
@@ -0,0 +1,13 @@
+#Nested Loop = The "inner loop" will finish all of it's iterations before
+#              finishing one iteration of the "outer loop"
+
+rows = int(input("How many rows?: "))
+columns = int(input("How many columns?: "))
+symbol = input("Enter a symbol to use: ")
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end="")
+    print()
+
+#, end="" is to prevent the symbol to print on the next line.
