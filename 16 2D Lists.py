be390acf3638c7582fa5ba135d12f40abe39f960
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-03 02:20:18 +0800

    2D Lists

---
+++ 16 2D Lists.py
@@ -0,0 +1,12 @@
+# 2D lists = a list of lists
+# aka Multi-dimensional/nested list
+
+
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza","hamburger","hotdog"]
+dessert = ["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food[0][2])
+#first [] is drink/dinner/dessert. second [] is the list within those 3 lists.
