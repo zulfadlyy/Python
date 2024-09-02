7dce3ae155bfc8e9fa144b2cc70dac0aee6f2e6a
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-02 21:05:39 +0800

    Lists

---
+++ 15 Lists.py
@@ -0,0 +1,18 @@
+# List = used to store multiple items in a single variable
+# Element = what each item in a list is called.
+
+food = ["pizza","hamburger","hotdog","spaghetti","pudding"]
+
+food[0] = "sushi"
+
+# print(food[0])
+
+# food.append("ice cream") add element at the end
+# food.remove("hotdog") remove specific element(s)
+# food.pop() remove last element
+# food.insert(0,"cake") add element in specific position in the list.
+# food.sort() sort alphabetically
+# food.clear() remove all elements in list
+
+for x in food:
+    print(x)
