fa61aaff4db16b74ceca6c7d82dbb21e3b1d2c61
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-04 15:39:50 +0800

    Return statement

---
+++ 22 Return Statement.py
@@ -0,0 +1,13 @@
+# Return Statement = used within functions to send Python values/objects
+#                    back to the caller. These values/objects are known as
+#                    the function's return value.
+
+def multiply(number1,number2):
+    return number1 * number2
+    # result = number1 * number2
+    #return result
+
+x = multiply(6,8)
+
+# print(multiply(6,8)
+print(x)
