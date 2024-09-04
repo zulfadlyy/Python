569ddfb8dd68dc17cdd8b9b4607fd2ef503378ad
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-04 15:34:01 +0800

    Functions

---
+++ 21 Function.py
@@ -0,0 +1,10 @@
+#Function = a block of code which is executed only when it is called.
+
+def hello(first_name,last_name,age): #parameters set up
+    print("Hello "+first_name+" "+last_name)
+    print("You are "+str(age)+" years old")
+    print("Have a nice day!")
+
+hello("Zack","Farsley",21) #arguments
+
+#We need matching parameters set up to receive the arguments
