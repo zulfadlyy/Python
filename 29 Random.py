afaa4f299b5675f6f589d5b4f0d8437dfe9f7461
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-08 10:03:36 +0800

    Random Module

---
+++ 29 Random.py
@@ -0,0 +1,19 @@
+import random
+
+#   Random int between (x,x)
+# x = random.randint(1,6)
+# print(x)
+
+#   Random number between 0 and 1 by default
+# y = random.random()
+# print(y)
+
+#   Random choice from list
+# myList = ['rock','paper','scissors']
+# z = random.choice(myList)
+# print(z)
+
+#   Randomly shuffle a list
+# cards = [1,2,3,4,5,6,7,8,9,"J","Q","K","A"]
+# random.shuffle(cards)
+# print(cards)
