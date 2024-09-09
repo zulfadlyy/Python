99e3b9da752916b65f6e87bd172ecb0eba56c3bf
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-09 15:30:24 +0800

    Reading a File

---
+++ 32 Reading a File.py
@@ -0,0 +1,14 @@
+
+#with open('test.txt') if the file is in the project directory.
+#if somewhere else, you need the project/file path.
+
+#with open and file.read will close the file automatically after reading it.
+
+
+try:
+    with open ("C:\\Users\\blazi\\Documents\\VSCODE\\File Detection\\test.txt") as file:
+        print(file.read())
+except FileNotFoundError: #Add exception handling incase not found or typo.
+    print("That file was not found :(")
+
+print(file.closed) #Checks if the file is closed.
