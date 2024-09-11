e0bf4d740cfcf8baeb308f315048e3bb1273f26c
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-12 05:39:08 +0800

    Modules

---
+++ 37 Modules.py
@@ -0,0 +1,8 @@
+# Module = a file containing python code. May contain functions, classes etc.
+# used with modular programming, which is to seperate a program into parts.
+
+import Messages as msg
+
+msg.hello()
+msg.bye()
+
