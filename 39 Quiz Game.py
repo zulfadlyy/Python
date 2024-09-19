13df8ca8ef5bf071dbd510ed231604df15cf33e4
zoofatlee <zulfadlyyusoff@gail.com>
2024-09-19 08:45:10 +0800

    Quiz Game

---
+++ 39 Quiz Game.py
@@ -0,0 +1,20 @@
+#------------------------
+def new_game():
+    pass
+#------------------------
+def check_answer():
+    pass
+#------------------------
+def display_score():
+    pass
+#------------------------
+def play_again():
+    pass
+#------------------------
+
+questions = {
+"Who created Python?: ": "A",
+"What year was Python created?: ": "B",
+"Python is tributed to which comedy group?: ": "C",
+"Is the Earth round?: ": "A"
+}
