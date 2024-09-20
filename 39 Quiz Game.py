86d5bd2f28b58dd1f7e96518dcde69b51d73aa26
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-21 02:25:29 +0800

    Quiz Game

--- 39 Quiz Game.py
+++ 39 Quiz Game.py
@@ -33,11 +33,28 @@ def display_score(correct_guesses, guesses):
     print("Answers: ", end="")
     for i in questions:
         print(questions.get(i), end="")
+    print()
+
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(i, end="")
+    print()
+
+    score = int((correct_guesses/len(questions)*100))
+    print("Your score is: "+ str(score)+"%")
         
 
 #------------------------
 def play_again():
-    pass
+    
+    response = input("Do you want to play again? (yes or no): ")
+    response = response.upper()
+
+    if response == "YES":
+        return True
+    else:
+        return False
+    
 #------------------------
 
 questions = {
@@ -53,3 +70,8 @@ options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates","D. Mark Zuck
            ["A. True", "B. False", "C. Sometimes", "D. What's Earth"]]
 
 new_game()
+
+while play_again(): 
+    new_game()
+
+print("Byeeeeeeee!")
