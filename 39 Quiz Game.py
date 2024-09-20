edcb39cfd7eef2c417e2e206c5ca7a0d21113f5b
zoofatlee <zulfadlyyusoff@gmail.com>
2024-09-21 02:04:34 +0800

    Quiz Game

--- 39 Quiz Game.py
+++ 39 Quiz Game.py
@@ -1,12 +1,40 @@
 #------------------------
 def new_game():
-    pass
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in questions:
+        print("------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter (A, B, C, or D): ")
+        guess = guess.upper()
+        guesses.append(guess)
+
+        correct_guesses += check_answer(questions.get(key),guess)
+        question_num+= 1
+    display_score(correct_guesses, guesses)
 #------------------------
-def check_answer():
-    pass
+def check_answer(answer, guess):
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
+    
 #------------------------
-def display_score():
-    pass
+def display_score(correct_guesses, guesses):
+    print("------------------------")
+    print("RESULTS")
+    print("------------------------")
+    print("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end="")
+        
+
 #------------------------
 def play_again():
     pass
@@ -18,3 +46,10 @@ questions = {
 "Python is tributed to which comedy group?: ": "C",
 "Is the Earth round?: ": "A"
 }
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates","D. Mark Zuckerburg"],
+           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+           ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+           ["A. True", "B. False", "C. Sometimes", "D. What's Earth"]]
+
+new_game()
